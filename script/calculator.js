5044012a02b8daa63a4331dc20110eba65b17ed0
Caesar-7 <giusemariotti77@gmail.com>
2020-09-25 18:16:56 +0200

    Fixed indentations

--- script/calculator.js
+++ script/calculator.js
@@ -3,45 +3,45 @@ n1 = op = n2 = res = point = isAns = null;
 let div = 1;
 
 function display() {
-	let screen = "";
+    let screen = "";
 
-	for (element of arguments) {
-		screen = `${screen}${element} `;
-	}
+    for (element of arguments) {
+        screen = `${screen}${element} `;
+    }
 
     document.getElementById("out").value = screen;
 }
 
 function set(num) {
-	if (!isAns) {
-		if (op == null) {
-			if (point) {
-				div /= 10;
-				n1 = num * div + n1;
-			} else {
-				n1 = num + n1 * 10;
-			}
+    if (!isAns) {
+        if (op == null) {
+            if (point) {
+                div /= 10;
+                n1 = num * div + n1;
+            } else {
+                n1 = num + n1 * 10;
+            }
 
-			display(n1);
-		} else {
-			if (point) {
-				div /= 10;
-				n2 = num * div + n2;
-			} else {
-				n2 = num + n2 * 10;
-			}
+            display(n1);
+        } else {
+            if (point) {
+                div /= 10;
+                n2 = num * div + n2;
+            } else {
+                n2 = num + n2 * 10;
+            }
 
-			display(n1, op, n2);
-		}
-	}
+            display(n1, op, n2);
+        }
+    }
 }
 
 function setop(operator) {
-	if (n1 != null) {
+    if (n1 != null) {
         op = operator;
 
-		point = false;
-		isAns = false;
+        point = false;
+        isAns = false;
         div = 1;
         n2 = null;
 
@@ -51,67 +51,67 @@ function setop(operator) {
 
 function float() {
     if (!point && !isAns) {
-		point = true;
-		div = 1;
+        point = true;
+        div = 1;
 
-		if (op == null) {
-			display(n1 + ".");
-		} else {
-			display(n1, op, n2 + ".")
-		}
-	}
+        if (op == null) {
+            display(n1 + ".");
+        } else {
+            display(n1, op, n2 + ".")
+        }
+    }
 }
 
 function ans() {
-	if (res && !isAns && !point) {
-		set(res);
+    if (res && !isAns && !point) {
+        set(res);
 
-    	isAns = true;
-	}
+        isAns = true;
+    }
 }
 
 function ce() {
     n1 = null;
-	op = null;
-	n2 = null;
-	div = 1;
+    op = null;
+    n2 = null;
+    div = 1;
     point = false;
 
     document.getElementById("out").value = 0;
 }
 
 function calculate() {
-	switch(op) {
-		case "+":
-			res = n1 + n2;
-			ce();
-			document.getElementById("out").value = res;
+    switch(op) {
+        case "+":
+            res = n1 + n2;
+            ce();
+            document.getElementById("out").value = res;
 
-			return;
-		case "-":
-			res = n1 - n2;
-			ce();
-			document.getElementById("out").value = res;
+            return;
+        case "-":
+            res = n1 - n2;
+            ce();
+            document.getElementById("out").value = res;
 
-			return;
-		case "*":
-			res = n1 * n2;
-			ce();
-			document.getElementById("out").value = res;
+            return;
+        case "*":
+            res = n1 * n2;
+            ce();
+            document.getElementById("out").value = res;
 
-			return;
-		case "/":
-			res = n1 / n2;
-			ce();
-			document.getElementById("out").value = res;
+            return;
+        case "/":
+            res = n1 / n2;
+            ce();
+            document.getElementById("out").value = res;
 
-			return;
-		case "**":
-			res = n1 ** n2;
-			ce();
-			document.getElementById("out").value = res;
+            return;
+        case "**":
+            res = n1 ** n2;
+            ce();
+            document.getElementById("out").value = res;
 
-			return;
-	}
+            return;
+    }
 }
 
